577517a37d3a40d357da48dcb08d1b4fa616b194
Ben Reinhart <breinhart@groupon.com>
2012-04-11 19:47:51 -0500

    Updated JS from class

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  // document is ready
+  var textFieldInput = $('#todo');
+
+  $('#submit').bind('click', function(){
+    var value = textFieldInput.val();
+
+    var deleteButton = ' <span class="delete">X</span>';
+
+    var listItem = '<li>' + value + deleteButton + '</li>';
+
+    var li = $(listItem);
+
+    $("#todos").append(li);
+    textFieldInput.val('');
+
+    var count = $('#todos').children('li').length;
+
+    $("#count").html(count);
+
+  });  
+
 
 });
